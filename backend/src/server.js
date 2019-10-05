ac3f3779b264f8ef7df77577d4777259ecc5714c
Michael Douglas Soares <soares.michael@gruposervices.com.br>
2019-10-04 23:50:16 -0300

    initial front app-air-cnc

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const routes = require('./routes');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
 
@@ -10,6 +11,8 @@ mongoose.connect('mongodb+srv://air-cnc:air-cnc@air-cnc-aqhz6.mongodb.net/air-cn
 // req.params = Acessar route params (para edição, delete)
 // req.body = Acessar corpo da requisição (para criação, edição)
 
+
+app.use(cors({ origin: 'http://localhost:3000' }));
 app.use(express.json());
 app.use(routes);
 
